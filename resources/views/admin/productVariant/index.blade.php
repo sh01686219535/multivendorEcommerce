f0c9c9dac93d8020ae1f146bcb0ef3c763676a7a
Md.Shariful islam <sharif.net888@gmail.com>
2024-08-13 10:47:47 -0700

    dddds

--- resources/views/admin/productVariant/index.blade.php
+++ resources/views/admin/productVariant/index.blade.php
@@ -7,35 +7,18 @@
         <div class="container">
             <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                 <div class="row">
-                    <h3><i class="fas fa-sliders-h icon-i m-2"></i> Product Upload Image</h3>
+                    <h3><i class="fas fa-sliders-h icon-i m-2"></i> Product Variant</h3>
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                         <div class="card">
                             <div class="card-head">
-                                <a href="{{route('admin.product.index')}}" class="m-2 btn btn-secondary">Back</a>
-                                <h3 class="m-2">Product Name : {{$product->name}}</h3>
-                                <form action="{{ route('admin.productImgGallery.store') }}" method="post"
-                                    enctype="multipart/form-data">
-                                    @csrf
-                                    <div class="row m-2">
-                                        <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8">
-                                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                                <input type="file" name="image[]" id="image" class="m-2"
-                                                    multiple />
-                                            </div>
-                                            <input type="hidden" name="product" value="{{ $product->id }}" />
-                                            @if ($product->thumb_image)
-                                                <img src="{{ asset($product->thumb_image) }}" width="100" height="100"
-                                                    class="rounded-circle" alt="" id="showImage">
-                                            @else
-                                                <img src="{{ asset('backendAsset/img/avatar5.png') }}" width="100"
-                                                    height="100" class="rounded-circle" alt="" id="showImage">
-                                            @endif
-                                        </div>
+                                <a href="{{ route('admin.product.index') }}" class="m-2 btn btn-secondary">Back</a>
+                                <div class="index d-flex justify-content-between m-3">
+                                    <h3 class="m-2">Product Name : {{ $product->name }}</h3>
+                                    <div>
+                                        <a data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                            class="btn btn-info">Create</a>
                                     </div>
-                                    <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 my-2">
-                                        <button type="submit" class="btn btn-success m-2">Submit</button>
-                                    </div>
-                                </form>
+                                </div>
                             </div>
                         </div>
                         <hr>
@@ -43,27 +26,24 @@
                             <div class="card-body">
                                 <div class="table-responsive">
                                     <table class="table table-hover table-bordered" id="example">
-                                     <thead>
-                                        <tr>
-                                            
-                                            <th>Sl</th>
-                                            <th>Product</th>
-                                            <th>Product Image</th>
-                                            <th>Action</th>
-                                        </tr>
-                                     </thead>
-                                     <tbody>
-                                       
-                                        <tr>
-                                           
-                                          
-                                         
-                                        </tr> 
-                               
-                                       
-                                     </tbody>
+                                        <thead>
+                                            <tr>
+                                                <th>Sl</th>
+                                                <th>Product</th>
+                                                <th>Product Image</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+                                        </tbody>
                                     </table>
-                                    
+
                                 </div>
                             </div>
                         </div>
@@ -72,19 +52,38 @@
             </div>
         </div>
     </div>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Variant</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Status</label>
+                        <select id="status" name="status" class="form-control">
+                            <option value="active">Active</option>
+                            <option value="Inactive">Inactive</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('js')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#image').change(function(e) {
-                var reader = new FileReader();
-                reader.onload = function(e) {
-                    $('#showImage').attr('src', e.target.result);
-                }
-                reader.readAsDataURL(e.target.files[0]);
-            });
-        });
-    </script>
 @endpush
